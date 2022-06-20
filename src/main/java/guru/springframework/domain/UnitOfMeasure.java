fbf31fd8af200b2385c3941102c5574feb7e74be
OoThan <mgoothan.psnl@gmail.com>
2022-06-20 14:27:06 +0800

    uom relationship with Ingredient

--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -2,24 +2,19 @@ package guru.springframework.domain;
 
 import lombok.Data;
 
-import javax.persistence.*;
-import java.math.BigDecimal;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  * Created By oothan on Jun, 2022
  */
 @Data
 @Entity
-public class Ingredient {
+public class UnitOfMeasure {
      @Id
      @GeneratedValue(strategy = GenerationType.IDENTITY)
      private Long id;
      private String description;
-     private BigDecimal amount;
-
-     @OneToOne(fetch = FetchType.EAGER)
-     private UnitOfMeasure uom;
-
-     @ManyToOne
-     private Recipe recipe;
 }
