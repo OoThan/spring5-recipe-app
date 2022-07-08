775054860c8ff0b1b65536c3c152473cbfed12e9
OoThan <mgoothan.psnl@gmail.com>
2022-07-08 19:52:37 +0630

    Spring MockMvc Test

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -6,6 +6,9 @@ import junit.framework.TestCase;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -14,6 +17,8 @@ import java.util.Set;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 /**
  * Created By oothan on Jul, 2022
@@ -33,6 +38,14 @@ public class IndexControllerTest extends TestCase {
         controller = new IndexController(recipeService);
     }
 
+    public void testMockMvc() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
     public void testGetIndexPage() {
         Set<Recipe> recipes = new HashSet<>();
         recipes.add(new Recipe());
@@ -44,7 +57,6 @@ public class IndexControllerTest extends TestCase {
         when(recipeService.getRecipes()).thenReturn(recipes);
 
         ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
-
 
         String viewName = controller.getIndexPage(model);
         assertEquals("index", viewName);
